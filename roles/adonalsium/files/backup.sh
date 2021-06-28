28a0233725d0f7647de5e7a076336ecdbff57299
FuzzyMistborn <fuzzymistborn@protonmail.com>
2021-06-27 22:46:34 -0400

    Add NFS shares for backup, tweaks, add elasticsearch db lxc

--- roles/adonalsium/files/backup.sh
+++ roles/adonalsium/files/backup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # Mount HDD
 mount -t ntfs /dev/disk/by-uuid/26967030966FFF1D /media/external_hdd
+mount /mnt/synology_backup
+mount /mnt/synology_minio
 
 # Wait for mount
-sleep 5s
+sleep 10s
 
 # rsync script
-rsync -Aavx --progress --delete --exclude 'UHD' --exclude 'Parents *' --exclude 'Kids TV' /mnt/Media/ /media/external_hdd/Media
-rsync -Aavx --progress --delete --exclude 'Coppermind' --exclude 'Kelsier' /mnt/Backup/ /media/external_hdd/Backup
+rsync -Aavx --progress --delete --exclude '.*' --exclude 'TV Shows' --exclude 'Parents *' --exclude 'UHD' /mnt/Media/ /media/external_hdd/Media
+rsync -Aavx --progress --delete --exclude '.*' --exclude '#recycle' /mnt/synology_backup/ /media/external_hdd/Backup
+rsync -Aavx --progress --delete --exclude '.*' --exclude 'media' /mnt/synology_minio/ /media/external_hdd/Minio
 
 # Wait for dismount
 sleep 10s
 
 # Unmount HDD
 umount /media/external_hdd
+umount /mnt/synology_backup
+umount /mnt/synology_minio 
 echo "Backup Complete!"
-echo
