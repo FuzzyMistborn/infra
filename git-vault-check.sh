84bea47d1dc8f5a555ba52c53b1ba7e5228ff580
FuzzyMistborn <fuzzymistborn@protonmail.com>
2021-10-19 00:19:49 -0400

    Add retroarch setup and Pi config

--- git-vault-check.sh
+++ git-vault-check.sh
@@ -3,7 +3,7 @@ green=$'\e[0;32m'
 red=$'\e[0;31m'
 reset=$'\e[0m'
 
-if ( cat vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
+if ( cat $HOME/infra/vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
 echo "${green}Vault Encrypted. Safe to commit.${reset}"
 else
 echo "${red}Vault not encrypted! Run 'make encrypt' and try again.${reset}"
--- roles/autonomy/files/scripts/auth_commands.sh
+++ roles/autonomy/files/scripts/auth_commands.sh
@@ -19,18 +19,6 @@ case "$SSH_ORIGINAL_COMMAND" in
     "./scripts/cast_stop.sh")
         ./scripts/cast_stop.sh
         ;;
-    "/usr/local/bin/snoo toggle")
-        /usr/local/bin/snoo toggle
-        ;;
-    "/usr/local/bin/snoo up")
-        /usr/local/bin/snoo up
-        ;;
-    "/usr/local/bin/snoo down")
-        /usr/local/bin/snoo down
-        ;;
-    "/usr/local/bin/snoo toggle_hold")
-        /usr/local/bin/snoo toggle_hold
-        ;;
     *)
         echo "Access denied"
         exit 1
