563a4e495e3ed775035336e0403cda13b938aeee
FuzzyMistborn <fuzzymistborn@protonmail.com>
2021-12-20 22:15:25 -0500

    tweak vault script

--- vault.sh
+++ vault.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-bw get password "Ansible Infra"
+if bw sync --nointeraction --session "$(cat /tmp/bw.token)" > /dev/null ; then
+    bw get password "Ansible Infra" --session "$(cat /tmp/bw.token)"
+else
+    bw unlock --raw > /tmp/bw.token
+    bw get password "Ansible Infra" --session "$(cat /tmp/bw.token)"
+fi
