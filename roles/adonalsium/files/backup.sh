756607573c8a034da84d888a28259472226f3061
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2023-07-02 00:04:34 -0400

    Switch to Restic-Rest server over Minio

--- roles/adonalsium/files/backup.sh
+++ roles/adonalsium/files/backup.sh
@@ -2,7 +2,7 @@
 # Mount HDD
 mount /dev/disk/by-uuid/54622b56-2601-4404-a519-2546242b0b42 /media/external_hdd
 mount /mnt/synology_backup
-mount /mnt/synology_minio
+mount /mnt/synology_restic
 
 # Wait for mount
 sleep 10s
@@ -10,7 +10,7 @@ sleep 10s
 # rsync script
 rsync -Aavx --progress --delete --exclude '.*' --exclude 'TV Shows' --exclude 'Parents *' --exclude 'UHD' /mnt/Media/ /media/external_hdd/Media
 rsync -Aavx --progress --delete --exclude '.*' --exclude '#recycle' /mnt/synology_backup/ /media/external_hdd/Backup
-rsync -Aavx --progress --delete --exclude '.*' --exclude 'media' /mnt/synology_minio/ /media/external_hdd/Minio
+rsync -Aavx --progress --delete --exclude '.*' --exclude 'media' /mnt/synology_restic/ /media/external_hdd/Minio
 
 # Wait for dismount
 sleep 10s
@@ -18,5 +18,5 @@ sleep 10s
 # Unmount HDD
 umount /media/external_hdd
 umount /mnt/synology_backup
-umount /mnt/synology_minio 
+umount /mnt/synology_restic
 echo "Backup Complete!"
