cb67fe578f3bb2022e491be9e8c07891c4bbb548
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2023-12-05 20:49:10 -0500

    Update drives/backup, add Calibre

--- roles/adonalsium/files/backup.sh
+++ roles/adonalsium/files/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Mount HDD
-mount /dev/disk/by-uuid/54622b56-2601-4404-a519-2546242b0b42 /media/external_hdd
+mount /dev/disk/by-id/ata-WDC_WD140EDGZ-11B1PA0_9MGWV2JJ-part1 /media/external_hdd
 mount /mnt/synology_backup
 mount /mnt/synology_restic
 
@@ -8,9 +8,9 @@ mount /mnt/synology_restic
 sleep 10s
 
 # rsync script
-rsync -Aavx --progress --delete --exclude '.*' --exclude 'TV Shows' --exclude 'Parents *' --exclude 'UHD' --exclude 'Frigate' --exclude 'Files/arch' /mnt/Media/ /media/external_hdd/Media
+rsync -Aavx --progress --delete --exclude '.*' --exclude 'TV Shows' --exclude 'Parents *' --exclude 'Frigate' /mnt/Media/ /media/external_hdd/Media
 rsync -Aavx --progress --delete --exclude '.*' --exclude '#recycle' /mnt/synology_backup/ /media/external_hdd/Backup
-rsync -Aavx --progress --delete --exclude '.*' --exclude 'media' /mnt/synology_restic/ /media/external_hdd/Minio
+rsync -Aavx --progress --delete --exclude '.*' --exclude 'media' /mnt/synology_restic/ /media/external_hdd/Restic
 
 # Wait for dismount
 sleep 10s
