d103f52f7894e59224e063cdd0b9b9374129dc3d
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2025-06-10 21:22:34 -0400

    Fix storage

--- roles/adonalsium/files/backup.sh
+++ roles/adonalsium/files/backup.sh
@@ -7,7 +7,7 @@ mount /mnt/local_backup
 sleep 10s
 
 # rsync script
-rsync -Aavx --info=progress2 --delete --delete-excluded --exclude '.*' --exclude '.trickplay' --exclude 'TV Shows' --exclude 'Parents *' --exclud 'UHD' --exclude 'Frigate' --exclude 'Immich' /mnt/Media/ /media/external_hdd/Media
+rsync -Aavx --info=progress2 --delete --delete-excluded --exclude '.*' --exclude '.trickplay' --exclude 'TV Shows' --exclude 'Parents *' --exclude 'UHD' --exclude 'Frigate' --exclude 'Immich' /mnt/Media/ /media/external_hdd/Media
 rsync -Aavx --info=progress2 --delete --delete-excluded --exclude '.*' --exclude '.snapraid.content' /mnt/local_backup/ /media/external_hdd/Backup
 
 # Wait for dismount
---
+++ roles/adonalsium/files/storage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ 
+sudo umount -f /mnt/pve/NFS
+sudo mount -a
